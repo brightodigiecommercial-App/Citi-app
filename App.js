0abd6cfa19595e7034a313f806bb1a47383c09a8
brightodigiecommercial-App <brightodigie.commercial@gmail.com>
2026-02-25 17:11:26 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { StatusBar } from 'expo-status-bar';
 import Navigation from './src/navigation/Navigation';
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <StatusBar style="dark" />
-        <Navigation />
-      </NavigationContainer>
-    </SafeAreaProvider>
-  );
+  return <Navigation />;
 }
